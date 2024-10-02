d640e5c207cbc1e5d82aece5be5aaf4397234a35
gyeolhwi <gyeori0626@gmail.com>
2024-10-02 12:11:17 +0900

    first commit

---
+++ src/test/java/com/starbucksorder/another_back/AnotherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbucksorder.another_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
