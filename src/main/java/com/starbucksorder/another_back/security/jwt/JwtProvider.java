13f27181a8c737f9037589095741366e5da8a3c0
gyeolhwi <gyeori0626@gmail.com>
2024-10-17 02:00:10 +0900

    jwt provider 로직생성-결휘

---
+++ src/main/java/com/starbucksorder/another_back/security/jwt/JwtProvider.java
@@ -0,0 +1,49 @@
+package com.starbucksorder.another_back.security.jwt;
+
+import com.starbucksorder.another_back.entity.Admin;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.Key;
+import java.util.Date;
+
+@Configuration
+public class JwtProvider {
+    private final Key key;
+
+    public JwtProvider(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    private Date getExpirationDate() {
+        return new Date(new Date().getTime() + 1000l * 60 * 60 * 3);
+    }
+
+    public String generateToken(Admin admin) {
+        return Jwts.builder()
+                .claim("adminId", admin.getAdminId())
+                .signWith(key, SignatureAlgorithm.HS256)
+                .expiration(getExpirationDate())
+                .compact();
+    }
+
+    public String removeBearerToken(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            throw new RuntimeException();
+        }
+        return token.substring("Bearer ".length());
+    }
+
+    public Claims parseToken(String token) {
+        JwtParser parser = Jwts.parser().setSigningKey(key).build();
+        return parser.parseClaimsJws(token).getPayload();
+    }
+
+
+}
