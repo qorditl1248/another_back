ed2961a4136b1e981847125fbfe70da74c7b76ac
gyeolhwi <gyeori0626@gmail.com>
2024-10-18 10:44:40 +0900

    필터동작 리팩토링, http.exceptionHandling 추가

--- src/main/java/com/starbucksorder/another_back/config/SecurityConfig.java
+++ src/main/java/com/starbucksorder/another_back/config/SecurityConfig.java
@@ -7,7 +7,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +28,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
-                .antMatchers("/auth/**","/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .antMatchers("/admin/**").authenticated();
+//                .anyRequest().authenticated();
 //                .antMatchers(HttpMethod.GET, "/menu/**","/**").permitAll()
         // exceptionHandling
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.setStatus(401);
+                    }
+                });
+
         // jwtFilter
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
