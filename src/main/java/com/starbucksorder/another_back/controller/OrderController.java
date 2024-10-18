b06671721a8f50e05702eee99140c1adb711f5f6
choiddanbi <eksql5955@naver.com>
2024-10-18 14:44:04 +0900

    CartService명 변경-단비

--- src/main/java/com/starbucksorder/another_back/controller/OrderController.java
+++ src/main/java/com/starbucksorder/another_back/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.starbucksorder.another_back.controller;
 
 import com.starbucksorder.another_back.dto.user.request.Order.ReqOrderDto;
-import com.starbucksorder.another_back.service.CartService;
+import com.starbucksorder.another_back.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @Autowired
-    private CartService cartService;
+    private OrderService orderService;
 
     // 주문목록 저장 ( 적립 안하는 사람은 userId = null )
     @PostMapping("/cart")
     public ResponseEntity<?> saveOrder(@RequestBody ReqOrderDto dto) {
-        cartService.saveOrder(dto);
+        orderService.saveOrder(dto);
         return ResponseEntity.ok().body(true);
     }
 }
