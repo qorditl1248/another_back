32e16a9bfbe03783be0c5ef1d480570ee9107f75
gyeolhwi <gyeori0626@gmail.com>
2024-10-22 11:28:36 +0900

    UPDATE 관리자 메뉴관리 메뉴 조회 및 검색에 대한 기능 취합 결휘

--- src/main/java/com/starbucksorder/another_back/repository/MenuMapper.java
+++ src/main/java/com/starbucksorder/another_back/repository/MenuMapper.java
@@ -1,6 +1,5 @@
 package com.starbucksorder.another_back.repository;
 
-import com.starbucksorder.another_back.dto.admin.MenuDto;
 import com.starbucksorder.another_back.entity.Menu;
 import com.starbucksorder.another_back.entity.Option;
 import org.apache.ibatis.annotations.Mapper;
@@ -32,14 +31,14 @@ public interface MenuMapper {
 
     // 메뉴리스트 전체 조회
     List<Menu> getMenuList();
-    int totalCount();
 
-    List<Menu> getMenuListPage(@Param("startIndex") Long startIndex, @Param("limit") Long limit);
+    int totalCount(@Param("searchName") String searchName);
+
+    // 관리자 메뉴관리 검색 및 페이지로 주기
+    List<Menu> getMenuListPageByName(Long startIndex, Long limit, String searchName);
 
     List<Option> getOptionList();
 
-    // 메뉴 검색
-    List<Menu> searchMenuByName(@Param("menuName") String menuName);
 
     // 메뉴 자소 분리 로직
     int updateMenuName(Long menuId, @Param("menuName") String menuName);
