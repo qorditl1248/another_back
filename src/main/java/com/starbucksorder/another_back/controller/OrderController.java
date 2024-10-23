ff475e8e501b5a43299751e8b3256421bf8961a2
gyeolhwi <gyeori0626@gmail.com>
2024-10-23 10:52:43 +0900

    TEST Order HACK: portone 도입 전 데이터 형식 확인 결휘

--- src/main/java/com/starbucksorder/another_back/controller/OrderController.java
+++ src/main/java/com/starbucksorder/another_back/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.starbucksorder.another_back.controller;
 
 import com.starbucksorder.another_back.dto.user.request.Order.ReqOrderDto;
+import com.starbucksorder.another_back.dto.user.request.Order.ReqOrderPortOneDto;
 import com.starbucksorder.another_back.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,5 +20,10 @@ public class OrderController {
     public ResponseEntity<?> saveOrder(@RequestBody ReqOrderDto dto) {
         orderService.saveOrder(dto);
         return ResponseEntity.ok().body(true);
+    }
+    // HACK: 포트원 도입 전 데이터 요청 확인
+    @PostMapping("/order")
+    public ResponseEntity<?> add(@RequestBody ReqOrderPortOneDto order){
+        return ResponseEntity.ok().body(order);
     }
 }
