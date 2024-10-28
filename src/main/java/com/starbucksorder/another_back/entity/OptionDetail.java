f17ba30717cf34725356cf93eb08c42a7d44eae3
gyeolhwi <gyeori0626@gmail.com>
2024-10-28 09:42:23 +0900

    UPDATE 사용자 메뉴 상세보기 resultMap 사용으로 인한 구문수정 결휘

--- src/main/java/com/starbucksorder/another_back/entity/OptionDetail.java
+++ src/main/java/com/starbucksorder/another_back/entity/OptionDetail.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class OptionDetail {
     private Long optionDetailId;
     @JsonIgnore
+    private Long optionId;
     private String optionDetailValue;
     private int optionDetailPrice;
 
