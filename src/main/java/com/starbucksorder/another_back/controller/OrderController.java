254d00fe0d6143d1dcef5c2b77a2bb27dfaf6bc3
gyeolhwi <gyeori0626@gmail.com>
2024-10-29 15:42:00 +0900

    FEATURE 결제요청 결제요청 및 사용자 적립요청까지 결휘

--- src/main/java/com/starbucksorder/another_back/controller/OrderController.java
+++ src/main/java/com/starbucksorder/another_back/controller/OrderController.java
@@ -1,29 +1,30 @@
 package com.starbucksorder.another_back.controller;
 
+import com.starbucksorder.another_back.aspect.annotation.Log;
 import com.starbucksorder.another_back.dto.user.request.Order.ReqOrderDto;
 import com.starbucksorder.another_back.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
     // 주문목록 저장 ( 적립 안하는 사람은 userId = null )
-    @PostMapping("/cart")
-    public ResponseEntity<?> saveOrder(@RequestBody ReqOrderDto dto) {
-        orderService.saveOrder(dto);
-        return ResponseEntity.ok().body(true);
-    }
     // HACK: 포트원 도입 전 데이터 요청 확인
     // HACK: ReqOrderPortOneDto -> ReqOrderDto 변경
-    @PostMapping("/order")
+    @Log
+    @PostMapping()
     public ResponseEntity<?> add(@RequestBody ReqOrderDto order){
+        orderService.saveOrder(order);
         return ResponseEntity.ok().body(order);
     }
+
 }
--- src/main/java/com/starbucksorder/another_back/service/OrderService.java
+++ src/main/java/com/starbucksorder/another_back/service/OrderService.java
@@ -2,12 +2,16 @@ package com.starbucksorder.another_back.service;
 
 import com.starbucksorder.another_back.dto.user.request.Order.ReqOrderDto;
 import com.starbucksorder.another_back.entity.Order;
+import com.starbucksorder.another_back.entity.OrderDetail;
+import com.starbucksorder.another_back.entity.User;
 import com.starbucksorder.another_back.repository.OrderDetailMapper;
 import com.starbucksorder.another_back.repository.OrderMapper;
+import com.starbucksorder.another_back.repository.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,12 +21,30 @@ public class OrderService {
     private OrderMapper orderMapper;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     // 주문목록 저장
     @Transactional(rollbackFor = RuntimeException.class)
     public void saveOrder(ReqOrderDto dto) {
         Order order = dto.toOrderEntity();
+        // 번호조회 먼저
+        User user = findUser(dto.getCustomer().getPhoneNumber());
+        if (user == null) {
+            user = dto.getCustomer().toUser();
+            // 번호등록
+            userMapper.saveUser(user);
+        }
+        order.setUserId(user.getUserId());
+        // 주문등록
         orderMapper.save(order);
-        orderDetailMapper.save(dto.getProducts().stream().map(product -> product.toOrderDetailEntity(order.getOrderId())).collect(Collectors.toList()));
+        // 주문 상세 등록
+        orderDetailMapper.save(order.getOrderDetails(), order.getOrderId());
+
+    }
+
+    public User findUser(String phoneNumber) {
+        User user = userMapper.findUserByPhoneNumber(phoneNumber);
+        return user;
     }
 }
